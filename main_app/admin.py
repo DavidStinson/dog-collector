9db1071ff97caeb4edcb81c12f82786ae43aed48
David Stinson <david.stinson.5@gmail.com>
2020-03-20 07:45:24 -0500

    Add Walk model

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 
 from django.contrib import admin
 # import your models here
-from .models import Dog
+from .models import Dog, Walks
 
 # Register your models here
 admin.site.register(Dog)
+admin.site.register(Walks)
--- main_app/models.py
+++ main_app/models.py
@@ -3,15 +3,33 @@ from django.urls import reverse
 
 # Create your models here.
 
+TIMES = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'),
+  ('E', 'Evening')
+)
+
 
 class Dog(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField() 
+  def __str__(self):
+    return self.name    
+  def get_absolute_url(self):
+    return reverse('dogs_detail', kwargs={'dog_id': self.id})
 
-    def __str__(self):
-        return self.name
 
-    def get_absolute_url(self):
-        return reverse('dogs_detail', kwargs={'dog_id': self.id})
+class Walks(models.Model):
+  date = models.DateField('Date Walked')
+  time = models.CharField(
+    max_length=1,
+    choices=TIMES,
+    default=TIMES[0][0]
+  )
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"

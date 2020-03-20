f3b0b5c4e0cd1c3a31c37b96c814a165ea837916
David Stinson <david.stinson.5@gmail.com>
2020-03-20 07:17:13 -0500

    Finish CBV

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Define the home view
@@ -21,3 +22,19 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['description', 'age']
+
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
